57c3e07532a6197009d2f258866aa7f56b56939d
Elizabeth <elizabeth.dev27@gmail.com>
2020-03-14 17:00:15 -0500

    auth

---
+++ backend/controllers/authController.js
@@ -0,0 +1,44 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+
+exports.authUser = async (req, res) => {
+  const errores = validationResult(req);
+
+  if( !errores.isEmpty() ) {
+    return res.status(400).json({errores:errores.array()})
+	}
+
+	const {email, password} = req.body;
+
+	try {
+		let usuario = await Usuario.findOne({email});
+		if(!usuario) {
+      return res.status(400).json({msg: 'El usuario no existe'});
+		}
+		
+		const passCorrecto = await bcryptjs.compare(password, usuario.password);
+		if(!passCorrecto) {
+      return res.status(400).json({msg: 'Password incorrecto'});
+		}
+
+		//crear y firmar jwt, si todo es correcto
+    const payload = {
+      usuario:{
+        id: usuario.id
+      }
+    };
+
+    jwt.sign(payload, process.env.FIRMA, {
+      expiresIn: 36000 //1 hora
+    }, (error, token) => {
+      if(error) throw error;
+
+      res.json({ token });
+    });
+
+	} catch (error) {
+		console.log(error);
+	}
+}
--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(express.json({extended: true}));
 const PORT = process.env.PORT || 4000;
 
 app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
 
 
 app.listen(PORT, () => {
